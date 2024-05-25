53dc9716b0affaa2db8763b62c491c7c3f1cd388
lynHong <15811082932@163.com>
2024-05-25 07:06:13 +0200

    add a timeline to the main menu

--- Assets/SlimUI/Modern Menu 1/Scripts/Audio/CheckMusicVolume.cs
+++ Assets/SlimUI/Modern Menu 1/Scripts/Audio/CheckMusicVolume.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace SlimUI.ModernMenu{
-	public class CheckMusicVolume : MonoBehaviour {
-		public void  Start (){
+namespace SlimUI.ModernMenu
+{
+	public class CheckMusicVolume : MonoBehaviour
+	{
+		public void Start()
+		{
 			// remember volume level from last time
+			Application.targetFrameRate = 90;
 			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
 		}
 
-		public void UpdateVolume (){
+		public void UpdateVolume()
+		{
 			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
 		}
 	}
